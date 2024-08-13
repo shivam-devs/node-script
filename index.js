65b58033822dba0574503b8ade075532a2103e5b
shivam-devs <bdash625@gmail.com>
2024-08-13 20:30:16 +0530

    Bhakk2

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import axios from "axios"
-
+import express from "express"
+const server = express();
 const api_call = async() =>{
     return axios.get("https://backend-96qf.onrender.com/api/questions").then((data)=>data)
 }
+let counter = 0;
 setInterval(()=>{
-    api_call().then((data)=>console.log(data.data.length))
+    api_call().then((data)=>console.log(data.data.length," data fetched for",++counter," times"))
 },15000)
 
+server.listen((8000),()=>{
+    console.log("Listing to port 8000");
+    
+})
